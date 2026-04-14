5e4e6d4b04317e27fd21caebcbff021b40c421e7
Travis Burke <tburke22@cscc.edu>
2026-04-14 11:29:48 -0400

    create fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,7 +93,15 @@ class AlienInvasion:
     def _create_fleet(self):
         """Create the fleet of aliens"""
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
 
 if __name__ == '__main__':
     # Make a game instance, and run the game
