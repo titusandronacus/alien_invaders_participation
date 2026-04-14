8992247fcc73a0d5d8129587d53893a2b00fabbb
Travis Burke <tburke22@cscc.edu>
2026-04-14 11:24:10 -0400

    limit number of bullets on screen at one time

--- alien_invasion.py
+++ alien_invasion.py
@@ -83,8 +83,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
